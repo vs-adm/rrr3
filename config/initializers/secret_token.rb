b12bfadb45c2470ed7ec4e5be9012a1dcfc9b40c
Vasily Shmelev <shade@shade-osx.local>
2011-08-18 21:38:25 +0000

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Electro::Application.config.secret_token = '7c65a6a7c5be12cd967fe3f1757c18ef99f3042a9c8bd38f947124bab26d448d6736da36c511c8298e6689601d9e6c0a01972072f4f04c71146a777d0a03e959'
